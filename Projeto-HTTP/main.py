8be603fb2d3006037c6ab2d16dfaaaae9eff146c
Alessandra Avelino <80168478+alessandraavelino@users.noreply.github.com>
2021-10-05 16:14:38 -0300

    Add files via upload

--- Projeto-HTTP/main.py
+++ Projeto-HTTP/main.py
@@ -1,13 +1,19 @@
+# Dupla: Alessandra Avelino // Hacjesse Barbosa
+
 from flask import Flask, render_template, request
 from json import dumps
 
 app = Flask(__name__)
+agenda = {}
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        return dumps(request.form)
+        #return dumps(request.form)
+        agenda[ request.form['ID'] ] = request.form
+        return render_template("lista.html", agenda = agenda.values())
     return render_template("index.html")
+
 
 if __name__== "__main__":
     app.run(debug=True)
